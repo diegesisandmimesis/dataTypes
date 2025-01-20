7d24550e98465498078b8df74f5de828e3311117
Diegesis & Mimesis <diegesisandmimesis@proton.me>
2025-01-20 15:25:09 -0800

    Removed unused scene code

--- dataStructures.h
+++ dataStructures.h
@@ -6,6 +6,11 @@
 // General definitions
 //
 
+#define DefineTypeCheck(cls) \
+#ifndef is##cls \
+#define is##cls(obj) {} \
+#endif
+
 // Utility define for testing an object to see if it is an instance of some
 // class
 #define isType(obj, cls) ((obj != nil) && obj.ofKind(cls))
@@ -17,12 +22,18 @@
 #define isRoom(obj) (isType(obj, Room))
 #define isActor(obj) (isType(obj, Actor))
 
-#define isObject(obj) ((obj != nil) && (dataType(obj) == TypeObject))
 #define isCollection(obj) (isType(obj, Collection))
+#define isList(obj) (isType(obj, List))
+#define isObject(obj) ((obj != nil) && (dataType(obj) == TypeObject))
+#define isVector(obj) (isType(obj, Vector))
 
-#ifndef isInteger
 #define isInteger(obj) ((obj != nil) && (dataType(obj) == TypeInt))
-#endif // isInteger
+
+// Like adv3's nilToList but for integers
+#define nilToInt(v, def) ((v == nil) ? def : toInteger(v))
+
+// Set obj to be v, but only if obj is currently nil
+#define noClobber(obj, v) (obj = (obj == nil) ? v : nil)
 
 // Define a PRNG test if we don't already have one.
 // This is a kludge-within-a-kludge:  we will ALWAYS return nil if isPRNG
@@ -35,14 +46,8 @@
 #define isPRNG(obj) ((obj != nil) && nil)
 #endif // isPRNG
 
-// Like adv3's nilToList but for integers
-#define nilToInt(v, def) ((v == nil) ? def : toInteger(v))
-
 // Get an object's outermost containing room
 #define gOutermostRoom(obj) (isThing(obj) ? obj.getOutermostRoom() : nil)
-
-// Set obj to be v, but only if obj is currently nil
-#define noClobber(obj, v) (obj = (obj == nil) ? v : nil)
 
 //
 // Graph definitions
@@ -145,24 +150,5 @@ Rule template 'ruleID';
 
 // Type tests
 #define isTrigger(obj) (isType(obj, Trigger))
-
-
-#ifdef SCENE
-
-#include "beforeAfter.h"
-#ifndef BEFORE_AFTER_H
-#error "The scene functions of this module require the beforeAfter module."
-#error "https://github.com/diegesisandmimesis/beforeAfter"
-#error "It should be in the same parent directory as this module.  So if"
-#error "dataStructures is in /home/user/tads/dataStructures, then"
-#error "beforeAfter should be in /home/user/tads/beforeAfter ."
-#endif // BEFORE_AFTER_H
-
-#define isScene(obj) ((obj != nil) && obj.ofKind(Scene))
-
-#endif // SCENE
-
-//TestCase template 'testCaseID'? [ testArgs ];
-//#define isTestCase(obj) ((obj != nil) && obj.ofKind(TestCase))
 
 #define DATA_STRUCTURES_H
